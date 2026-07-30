2b32867cc74217218073d5e49b26e83a8e331483
Manoel Campos <manoel31maycon@gmail.com>
2026-07-30 11:06:18 -0400

    Melhorando logica de troca de filial e Adicionando o tempo de execução

--- main.py
+++ main.py
@@ -3,19 +3,21 @@ from fluxo import *
 df_base_ra = pd.read_excel(r"C:\Users\manoel.campos\OneDrive - SFIEMT\Área de Trabalho\AutomatizacaoERP\Base_RAs\022__alunos_com_informacao_de_parceria_2026-07-27T16_00_39.4344432-04_00.xlsx", dtype={"RegistroAluno": str, "CodFilialSGE": str})
 cod_filial_unique = df_base_ra["CodFilialSGE"].drop_duplicates().tolist()
 
+tempo_inicial = time.time()
 eh_primeira_filial = True
+
 for filial in cod_filial_unique:
     df_filtrada = df_base_ra[df_base_ra["CodFilialSGE"] == filial]
     #Status zero é o primeiro laço do loop
     primeiro_ra = True
        
-    for RA in df_filtrada ["RegistroAluno"]:
+    for RA in df_filtrada["RegistroAluno"]:
         if primeiro_ra:
 
-            if eh_primeira_filial == False:
+            if eh_primeira_filial:
+                primeira_filial(filial)
+            else:
                 trocar_filial(filial)
-
-            primeira_filial(filial)
 
             time.sleep(1)
             filtro_aluno(RA)
@@ -28,12 +30,18 @@ for filial in cod_filial_unique:
             ajuste_campo_complementar()
             primeiro_ra = False
 
-    else:
-            trocando_RA(RA)
-            time.sleep(1)
+        else:
+                trocando_RA(RA)
+                time.sleep(1)
 
-            filtrando_curso()
-            time.sleep(1)
+                filtrando_curso()
+                time.sleep(1)
 
-            ajuste_campo_complementar()
+                ajuste_campo_complementar()
     eh_primeira_filial = False
+
+tempo_decorrido = time.time() - tempo_inicial
+minutos = int(tempo_decorrido //60)
+segundos = tempo_decorrido % 60
+
+print(f"Tempo total: {minutos} min {segundos:.2f} s")
